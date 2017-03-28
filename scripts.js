a37711a23448bbf4f1f6b75c6a9903383f4337ba
Jessica H <Jessica H>
2017-03-28 03:51:39 -0400

    WIP

---
+++ scripts.js
@@ -0,0 +1,56 @@
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash)
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']')
+			if (target.length) {
+				$('html, body').animate({
+			 	scrollTop: target.offset().top
+				}, 1000)
+				return false
+			}
+		}
+	})
+
+	$('#about-link').on('click', function() {
+		$('html, body').animate({
+		 	scrollTop: $('#about').offset().top
+		}, 1000)
+	})
+
+	$('#portfolio-link').on('click', function() {
+		$('html, body').animate({
+		 	scrollTop: $('#portfolio').offset().top
+		}, 1000)
+	})
+
+	$('#contact-link').on('click', function() {
+		$('html, body').animate({
+		 	scrollTop: $('#contact').offset().top
+		}, 1000)
+	})
+
+	$('.tothetop a').on('click', function() {
+		$('html, body').animate({
+		 	scrollTop: $('#top').offset().top
+		}, 1000)
+	})
+
+	//
+
+	$('.hidden-menu i').on('click', function() {
+		$('nav ul').css('top', '0').addClass('bg-overlay')
+		$('.hidden-menu').fadeOut()
+		$('.fa-times').fadeIn()
+	})
+
+	$('.fa-times').on('click', function() {
+		$('nav ul').css('top', '-100%').removeClass('bg-overlay')
+		$('.hidden-menu').fadeIn()
+		$('.fa-times').fadeOut()
+	})
+
+	//
+
+
+});
