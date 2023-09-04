96ad4e05c631ce82aa4d5e3c89254829ff9ac892
Jessica <jessica.ho@live.com>
2023-09-03 23:03:28 -0400

    install vite and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
 module.exports = {
 	darkMode: 'class',
 	content: [
-		'./pages/**/*.{html,js,vue}',
-		'./components/**/*.{html,js,vue}',
+		'./index.html',
+		"./src/**/*.{html,js}",
 	],
 	theme: {
-		container: {
-			center: true,
-			padding: '4rem'
-		},
-		colors: {
-			'green-100': '#87C29E',
-			'green-200': '#497563',
-			black: '#2E2E2E',
-			white: '#F2F2F2'
-		},
 		extend: {},
 	},
 	plugins: [],
